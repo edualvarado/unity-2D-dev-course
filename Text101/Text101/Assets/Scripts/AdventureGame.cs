3b58db6e56afb804f6482d13e183b237cb8e0ed5
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-06-18 11:47:46 +0200

    Upload Text101 project

---
+++ Text101/Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.getStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState2();
+ 
+    }
+
+    private void ManageState2()
+    {
+        var nextStates = state.getNextStates();
+        for(int idx = 0; idx < nextStates.Length; idx++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + idx))
+            {
+                state = nextStates[idx];
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+        textComponent.text = state.getStateStory();
+    }
+     
+    private void ManageState()
+    {
+        var nextStates = state.getNextStates(); // var is equal to State[]
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+
+        textComponent.text = state.getStateStory();
+    }
+}
---
+++ Text101/Text101/Assets/Scripts/State.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// For ScriptableObject
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    // SerializeField to make it visible to inspector
+    [TextArea(20,24)][SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+
+    public string getStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] getNextStates()
+    {
+        return nextStates;
+    }
+
+}
