ffc7860a148b7c8a6ebdae72c840caa2e4c73389
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-06-18 11:43:10 +0200

    Add Hello World and Number Wizard Console projects

---
+++ Intro_Setup/Hello World/Assets/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello World!");
+        print("My name is Edu!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    int max;
+    int min;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        Debug.Log("========================");
+        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("Pick a random number, don't tell me what it is... ");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than " + guess + "...");
+        Debug.Log("Push Up = higher, Push Down = lower, Enter = Correct!");
+        max = max + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Detect when the up arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed!");
+            min = guess;
+            NextGuess();
+        }
+
+        //Detect when the down arrow key is pressed down
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+            max = guess;
+            NextGuess();
+        }
+
+        //Detect when the enter key is pressed down
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("I predicted correctly!");
+            StartGame();
+        }
+
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("It is higher or lower than " + guess + "?");
+    }
+}
